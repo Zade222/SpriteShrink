2e4d334e83732fc201e979e5119f51e47dbd5882
Zadeis <tyler.bibeault@pm.me>
2025-08-04 12:14:47 -0400

    Revert "Update build.rs"
    This reverts commit cf0f97dcf3d8169ee2b092c5f297d9c344226785.

--- crates/lib_sprite_shrink/build.rs
+++ crates/lib_sprite_shrink/build.rs
@@ -4,26 +4,27 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    let crate_dir = env::var("CARGO_MANIFEST_DIR")
-        .expect("CARGO_MANIFEST_DIR env var is not set");
-    let crate_path = PathBuf::from(crate_dir);
-
-    let config_path = crate_path.join("../../cbindgen.toml");
-    let config = cbindgen::Config::from_file(&config_path)
-        .unwrap_or_else(|_| panic!("Failed to load cbindgen.toml from: {:?}", config_path));
-
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let package_name = env::var("CARGO_PKG_NAME").unwrap();
     let output_file = target_dir()
-        .join(format!("{}.h", package_name));
+        .join(format!("{}.h", package_name))
+        .to_str()
+        .unwrap()
+        .to_string();
+
+    let config = cbindgen::Config::from_file("cbindgen.toml").expect("Failed to load cbindgen.toml");
 
     cbindgen::Builder::new()
-      .with_crate(crate_path)
+      .with_crate(&crate_dir)
       .with_config(config)
       .generate()
       .expect("Unable to generate C bindings")
       .write_to_file(&output_file);
 }
 
+/// Find the location of the `target/` directory. Note that this may be 
+/// overridden by `cmake`, so we also need to check the `CARGO_TARGET_DIR` 
+/// variable.
 fn target_dir() -> PathBuf {
     if let Ok(target) = env::var("CARGO_TARGET_DIR") {
         PathBuf::from(target)
