38c45f63790d7124d65735015828812d02df047a
Zadeis <tyler.bibeault@pm.me>
2025-08-04 13:18:02 -0400

    Minor changes to build.rs.

--- crates/lib_sprite_shrink/build.rs
+++ crates/lib_sprite_shrink/build.rs
@@ -22,9 +22,6 @@ fn main() {
       .write_to_file(&output_file);
 }
 
-/// Find the location of the `target/` directory. Note that this may be 
-/// overridden by `cmake`, so we also need to check the `CARGO_TARGET_DIR` 
-/// variable.
 fn target_dir() -> PathBuf {
     if let Ok(target) = env::var("CARGO_TARGET_DIR") {
         PathBuf::from(target)
